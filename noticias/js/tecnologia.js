7ff8a84b4f57e5fc9d05d3311009dbdd1ec7d7d1
carolfps <37373110+carolfps@users.noreply.github.com>
2020-05-22 19:03:42 -0300

    corrigindo endereco api tecnologia

--- noticias/js/tecnologia.js
+++ noticias/js/tecnologia.js
@@ -2,7 +2,7 @@ let containerDeNoticias = document.getElementById('listaDeNoticias')
 
 async function pegarNoticiasTec(){
     
-    let noticias = await fetch('https://newsapi.caffsy.com/api/ProxyNewsApi?code=Tb5vwO2hATlmSFK5puLfqDjUclBbDIqxToxJ2AJ0cZlfyhzUpeqMGA==&country=br&category=science&category=technology')
+    let noticias = await fetch('https://newsapi.caffsy.com/api/ProxyNewsApi?code=Tb5vwO2hATlmSFK5puLfqDjUclBbDIqxToxJ2AJ0cZlfyhzUpeqMGA==&country=br&category=technology')
     
     let listaDeNoticias = await noticias.json()
 
