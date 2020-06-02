52633b1af458bef38b8c4ba8e72aa222cc53f143
carolfps <37373110+carolfps@users.noreply.github.com>
2020-06-02 16:57:42 -0300

    inserindo fotos na página

--- fotos-api/js/index.js
+++ fotos-api/js/index.js
@@ -7,14 +7,19 @@ async function pegarFotos(){
     let fotosJson = await fotos.json();
     
     for( let fotoId of fotosJson.photos.photo.map( p => p.id)){
-        
+
         let endFoto = await fetch ('https://api.flickr.com/services/rest/?method=flickr.photos.getSizes&api_key=7189f21f89748e26ea24e06206872cc8&photo_id=' + fotoId + '&format=json&nojsoncallback=1');
         
         let endFotoJson = await endFoto.json();
 
-        let srcFotoPeq = endFotoJson.sizes.size[5];
+        let srcFotoPeq = endFotoJson.sizes.size[1].source;
 
-        console.log(srcFotoPeq);
+        let img = document.createElement('img');
+        img.setAttribute('src', srcFotoPeq);
+        img.setAttribute('class', 'col-sm-2 my-2')
+
+        divFotos.appendChild(img);
+        
     }
 
 }
