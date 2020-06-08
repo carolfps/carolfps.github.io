d23045a538378616e98a6c810783ca9458f93f9f
carolfps <37373110+carolfps@users.noreply.github.com>
2020-06-08 14:39:15 -0300

    novo layout no card

--- todo-react/Card.jsx
+++ todo-react/Card.jsx
@@ -1,17 +1,20 @@
 const Card = (props) => {
   return (
-    <div className="card bg-light m-3 w-25">
-      <div className="card-header font-weight-bold">{props.todo.titulo}</div>
-      <div className="card-body d-flex justify-content-between align-items-center p-4">
-        <p className="card-text">{props.todo.descricao}</p>
-        <span
-          className="text-danger deletar"
-          onClick={(e) => props.handler(props.todo)}
-        >
-          <svg viewBox="0 0 24 24" focusable="false" aria-hidden="true" className="MuiSvgIcon-root">
-            <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zm2.46-7.12l1.41-1.41L12 12.59l2.12-2.12 1.41 1.41L13.41 14l2.12 2.12-1.41 1.41L12 15.41l-2.12 2.12-1.41-1.41L10.59 14l-2.13-2.12zM15.5 4l-1-1h-5l-1 1H5v2h14V4z" />
-          </svg>
-        </span>
+    <div className="card m-3 w-25">
+      <div className="card-body">
+          <h3 className="font-weight-bold">{props.todo.titulo}</h3>
+          <div className="d-flex justify-content-between align-items-center py-2">
+            <p className="card-text my-0">{props.todo.descricao}</p>
+            <span
+              className="text-danger deletar"
+              onClick={(e) => props.handler(props.todo)}
+              title="Deletar tarefa"
+            >
+              <svg viewBox="0 0 24 24" focusable="false" aria-hidden="true" className="MuiSvgIcon-root">
+                <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zm2.46-7.12l1.41-1.41L12 12.59l2.12-2.12 1.41 1.41L13.41 14l2.12 2.12-1.41 1.41L12 15.41l-2.12 2.12-1.41-1.41L10.59 14l-2.13-2.12zM15.5 4l-1-1h-5l-1 1H5v2h14V4z" />
+              </svg>
+            </span>
+          </div>
       </div>
     </div>
   );
--- todo-react/Navbar.jsx
+++ todo-react/Navbar.jsx
@@ -2,10 +2,8 @@ class Navbar extends React.Component{
     render(){
         return(
             <header>
-                <nav className="navbar navbar-dark bg-warning">
-                    <div className="container">
-                        <span className="text-white my-1">Lista de tarefas 📝</span>
-                    </div>
+                <nav className="navbar navbar-dark bg-warning py-1">
+                    <span className="navbar-brand my-1">Lista de tarefas 📝</span>
                 </nav>
             </header>
         )
