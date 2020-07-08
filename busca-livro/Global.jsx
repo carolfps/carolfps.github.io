10c2a0c5ce91068ede0ff5195a60cdd40c002405
carolfps <37373110+carolfps@users.noreply.github.com>
2020-07-08 17:34:57 -0300

    busca livro api google (json)

--- busca-livro/Global.jsx
+++ busca-livro/Global.jsx
@@ -1,7 +1,39 @@
 class Global extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            query: ''
+        }
+    }
+    search() {
+        const base_url = 'https://www.googleapis.com/books/v1/volumes?q='
+        fetch(`${base_url}${this.state.query}`, {method: 'GET'})
+        .then(response => response.json())
+        .then(json => console.log(json))
+    }
     render() {
         return(
-            <div>Componente global</div>
+            <div className="global">
+                <h2>Busca Livro</h2>
+                <div className="form-group">                    
+                    <div className="input-group">
+                        <input 
+                            type="text" 
+                            className="form-control" 
+                            placeholder="Busque um livro" 
+                            onChange={event => this.setState({query: event.target.value})}
+                            onKeyUp={event => {
+                                if(event.key == 'Enter'){
+                                    this.search();
+                                }
+                            }}
+                        />
+                        <div className="input-group-append" onClick={() => this.search()}>
+                            <span className="material-icons input-group-text">search</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
