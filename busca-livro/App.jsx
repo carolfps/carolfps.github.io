af30711198443e0f9224a011fa690f5613eb772d
carolfps <37373110+carolfps@users.noreply.github.com>
2020-07-08 16:46:45 -0300

    primeiro commit

---
+++ busca-livro/App.jsx
@@ -0,0 +1,11 @@
+class App extends React.Component  {
+    render() {
+      return (
+      <div> 
+        
+        <Global />
+      
+      </div>
+    );
+    }
+  }
---
+++ busca-livro/Global.jsx
@@ -0,0 +1,7 @@
+class Global extends React.Component{
+    render() {
+        return(
+            <div>Componente global</div>
+        )
+    }
+}
