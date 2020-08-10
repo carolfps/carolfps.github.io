3bc27128effd490675159f678dbb9f3a731ee6f1
carolfps <37373110+carolfps@users.noreply.github.com>
2020-08-10 10:15:08 -0300

    ajuste do responsivo

--- todo-react/Footer.jsx
+++ todo-react/Footer.jsx
@@ -2,10 +2,10 @@ const Footer = (props) => {
 
     return(
         <footer>
-            <form onSubmit={(e) => props.handler(e)} className="d-flex justify-content-center py-2 container">   
-                <input placeholder="Título" className="form-control col-md-5 mx-2" name="titulo" />    
-                <input placeholder="Descrição" className="form-control col-md-5 mx-2" name="descricao" />
-                <button className="btn-adicionar">Enviar</button>
+            <form onSubmit={(e) => props.handler(e)} className="d-flex flex-wrap justify-content-center py-2 container">   
+                <input placeholder="Título" className="form-control col-md-4 mx-2 my-1" name="titulo" />    
+                <input placeholder="Descrição" className="form-control col-md-4 mx-2 my-1" name="descricao" />
+                <button className="btn-adicionar col-lg-1 col-md-2 mx-2 my-1 py-1">Enviar</button>
             </form>
         </footer>
     )
